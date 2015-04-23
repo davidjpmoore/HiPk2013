3fbac39067553d2d0f5b3413bf4f9ede51865f79
davidjpmoore <davidjpmoore@arizona.edu>
2015-04-23 15:10:06 -0700

    attempt to merge via plyr

--- 03MergeENZ_BGC.R
+++ 03MergeENZ_BGC.R
@@ -20,6 +20,29 @@ unique(ENZdat$SITE)
 
 summary(BGCdat)
 
+
+library(plyr)
+EffluxByPLOTID <- ddply(FLUXdat, c("PLOTID","rDate"), summarise,
+                     N    = length(EFFLUX),
+                     Effluxmean = mean(EFFLUX),
+                     Effluxsd   = sd(EFFLUX),
+                     Effluxse   = sd / sqrt(N),
+                     Tmean = mean(Tsoil_C)
+                     )
+EffluxByPLOTID
+
+
+library(plyr)
+sumdata <- ddply(FLUXdat, c("PLOTID","rDate"), summarise,
+                     N    = length(EFFLUX),
+                     mean = mean(EFFLUX),
+                     sd   = sd(EFFLUX),
+                     se   = sd / sqrt(N) )
+sumdata
+
+
+
+
 # Trying to make temp subset dataframes of UMC, LSP and SPR from ENZdat and BGCdat
 temp1 <- filter(ENZdat, SITE %in% c("UMC", "LSP", "SPR"))
 head(temp1)
@@ -27,8 +50,18 @@ unique(temp1$SITE)
 View (temp2)
 temp2 <- filter(BGCdat, SITE %in% c("UMC", "LSP", "SPR"))
 
-temp.join <- full_join( temp1, temp2, by= "PLOTID")
+#
+junk1 =group_by(temp1,PLOTID)
+junk2 =group_by(temp2,PLOTID)
+
+
+#you can use join functions using multiple vectors
+#http://stackoverflow.com/questions/26611717/can-dplyr-join-on-multiple-columns-or-composite-key
+
+temp.join <- full_join( temp1, temp2, by= c("PLOTID","rDate"))
 View(temp.join)
+
+left_join(d1, d2, by = c("x" = "x2", "y" = "y2"))
 
 # Example of how to use tapply
 # #calculate weekly and daily values for plots
