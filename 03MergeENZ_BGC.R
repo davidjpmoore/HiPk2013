d629c589ec5852f179cc0d596e31c4f999152dbd
davidjpmoore <davidjpmoore@arizona.edu>
2015-08-18 06:52:30 -0700

    I've added some quick merge functions that have some issues.
    The merged files are ok for graphs but not statistics as there are multiple entries brought about by misalignment.

--- 03MergeENZ_BGC.R
+++ 03MergeENZ_BGC.R
@@ -27,12 +27,14 @@ summary(BGCdat)
 library(plyr)
 EffluxByPLOTID <- ddply(FLUXdat, c("PLOTID","rDate"), summarise,
                      N    = length(EFFLUX),
-                     Effluxmean = mean(EFFLUX),
-                     Effluxsd   = sd(EFFLUX),
+                     Effluxmean = mean(EFFLUX,na.rm=TRUE) ,
+                     Effluxsd   = sd(EFFLUX,na.rm=TRUE),
                      Effluxse   = sd / sqrt(N),
                      Tmean = mean(Tsoil_C)
                      )
 EffluxByPLOTID
+
+
 
 
 library(plyr)
@@ -40,29 +42,48 @@ sumdata <- ddply(FLUXdat, c("PLOTID","rDate"), summarise,
                      N    = length(EFFLUX),
                      mean = mean(EFFLUX),
                      sd   = sd(EFFLUX),
-                     se   = sd / sqrt(N) )
+                     se   = sd / sqrt(N),
+                      Tmean = mean(Tsoil_C)
+                 )
 sumdata
 
 
 
 
 # Trying to make temp subset dataframes of UMC, LSP and SPR from ENZdat and BGCdat
+
+#try grouping by week
+unique(FLUXdat$WEEK)
+
 temp1 <- filter(ENZdat, SITE %in% c("UMC", "LSP", "SPR"))
 head(temp1)
 unique(temp1$SITE)
 View (temp2)
 temp2 <- filter(BGCdat, SITE %in% c("UMC", "LSP", "SPR"))
+temp3 <- filter(FLUXdat, SITE %in% c("UMC", "LSP", "SPR"))
 
 #
 junk1 =group_by(temp1,PLOTID)
 junk2 =group_by(temp2,PLOTID)
+junk3 =group_by(temp3,PLOTID)
+unique(junk3$rDate)
+unique(junk2$rDate)
+
+unique(FLUXdat$rDate)
+unique(BGCdat$rDate)
 
 
 #you can use join functions using multiple vectors
 #http://stackoverflow.com/questions/26611717/can-dplyr-join-on-multiple-columns-or-composite-key
 
-temp.join <- full_join( temp1, temp2, by= c("PLOTID","rDate"))
+temp.join <- full_join( temp1, temp2, temp3, by= c("PLOTID","WEEK"))
 View(temp.join)
+
+
+temp2.join <- full_join( temp.join, temp3, by= c("PLOTID","WEEK"))
+
+T2join.lowpH <- subset(temp2.join, temp2.join$pH<5) 
+enzJunk1.lowpH <- subset(enzJunk1, temp2.join$pH<5)
 
 left_join(d1, d2, by = c("x" = "x2", "y" = "y2"))
 
